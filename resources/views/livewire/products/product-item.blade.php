feea69e10528d0887efde7fca8ec681ca00ff2f4
Najeeb Anwari <najeeb.anwari@gmail.com>
2024-12-03 22:37:29 +0430

    fix currency and money display issue in product item

--- resources/views/livewire/products/product-item.blade.php
+++ resources/views/livewire/products/product-item.blade.php
@@ -2,38 +2,38 @@
     <div class="card card-hover">
         <a href="/products/{{ $product->id }}">
 
-            <img src="{{ asset('storage/'. $product->images->first()->path . '/' . $product->images->first()->name)  }}"
+            <img src="{{ asset('storage/' . $product->images->first()->path . '/' . $product->images->first()->name) }}"
                 class="card-img-top" alt="...">
         </a>
         <div class="card-body">
             <a href="/products/{{ $product->id }}">
                 <h6 class="mb-3">{{ $product->name }}</h6>
             </a>
-            <div class="d-flex gap-3 mb-3 align-items-center" >
-                <del class="text-muted" x-show="{{ $product->old_price ? true : false }}" >؋{{ $product->old_price }}</del>
-                <h6 class="mb-0">؋{{ $product->price }}</h6>
+            <div class="d-flex gap-3 mb-3 align-items-center">
+                <del class="text-muted"
+                    @if ($product->old_price != null && $product->old_price != 0) style="display: block;" @else style="display: none;" @endif>؋{{ $product->old_price }}</del>                <h6 class="mb-0">؋{{ $product->price }}</h6>
             </div>
             @livewire('components.review-rating', ['product_id' => $product->id])
             @can('modify_cart')
-            <div class="d-flex">
-                <button wire:click="$emit('addCart',{{ $product->id }})" class="btn btn-primary btn-sm">Add to
-                    <i class="bi bi-cart"></i></button>
-                @if($wishedByBuyer)
-                    <a wire:click="removeWish" href="#" class="btn ms-auto">
-                        <i class="bi bi-heart-fill text-danger"></i>
-                    </a>
-                @else
-                    <a wire:click="addWish" href="#" class="btn ms-auto">
-                        <i class="bi bi-heart"></i>
-                    </a>
-                @endif
-            </div>
+                <div class="d-flex">
+                    <button wire:click="$emit('addCart',{{ $product->id }})" class="btn btn-primary btn-sm">Add to
+                        <i class="bi bi-cart"></i></button>
+                    @if ($wishedByBuyer)
+                        <a wire:click="removeWish" href="#" class="btn ms-auto">
+                            <i class="bi bi-heart-fill text-danger"></i>
+                        </a>
+                    @else
+                        <a wire:click="addWish" href="#" class="btn ms-auto">
+                            <i class="bi bi-heart"></i>
+                        </a>
+                    @endif
+                </div>
             @endcan
             @can('update', $product)
-            <div class="d-flex">
-                <a href="/products/{{ $product->id }}/edit" class="btn btn-primary btn-sm">
-                    <i class="bi bi-pencil"></i> Edit Product</a>
-            </div>
+                <div class="d-flex">
+                    <a href="/products/{{ $product->id }}/edit" class="btn btn-primary btn-sm">
+                        <i class="bi bi-pencil"></i> Edit Product</a>
+                </div>
             @endcan
         </div>
 
